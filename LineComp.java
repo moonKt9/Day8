8c159d3a9efbe044c24f2654538ba4385a7eff43
Kritika <kritistarsmind@gmail.com>
2022-05-06 15:43:41 +0530

    Added:lenth calculation

--- LineComp.java
+++ LineComp.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
 public class LineComp {
 public static void main(String[] args) 
 {
-	System.out.println("Welcome to the line comparision  computation program");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter first co-ordinate:");
+	int x1=sc.nextInt();
+	int y1=sc.nextInt();
+	System.out.println("Enter second co-ordinate:");
+	int x2=sc.nextInt();
+	int y2=sc.nextInt();
+	double length;
+	length=Math.sqrt((Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+	System.out.println("The lenth of line is:"+length);
 }
 
 }
